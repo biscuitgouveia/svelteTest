a2a214105c70d47624236f572de108d974d76b5d
J273884 <J273884@tafe.wa.edu.au>
2024-12-16 23:28:25 +0800

    Transfer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
-    import "../app.css";
-</script>
-
-<slot />
+    import "../app.css"
+    interface Props {
+       children?: import('svelte').Snippet;
+    }
+ 
+    let { children }: Props = $props();
+ </script>
+ 
+ <div class="min-h-screen flex flex-col">
+     {@render children?.()}
+ </div>
